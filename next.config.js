247feae32b3518bb73d4a5c7347ae521a88c3baa
محمد دربندی <darbandi.craft@gmail.com>
2022-04-28 08:25:40 +0430

    feat: add PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,46 @@
 /** @type {import('next').NextConfig} */
+
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const withPWA = require('next-pwa');
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['i.pravatar.cc'],
   },
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  },
+  plugins: [
+    new SWPrecacheWebpackPlugin({
+      minify: true,
+      staticFileGlobsIgnorePatterns: [/\.next\//],
+      runtimeCaching: [
+        {
+          handler: 'networkFirst',
+          urlPattern: /^https?.*/,
+        },
+      ],
+    }),
+  ],
+  // webpack: (config) => {
+  //   config.plugins.push(
+  //     new SWPrecacheWebpackPlugin({
+  //       minify: true,
+  //       staticFileGlobsIgnorePatterns: [/\.next\//],
+  //       runtimeCaching: [
+  //         {
+  //           handler: 'networkFirst',
+  //           urlPattern: /^https?.*/,
+  //         },
+  //       ],
+  //     })
+  //   );
+  //   return config;
+  // },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="manifest" href="/manifest.json" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
